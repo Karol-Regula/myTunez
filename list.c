e3211cc6c792afe65971158bf3bd38104e773b72
Karol-Regula <kregula@stuy.edu>
2016-10-17 11:49:40 -0400

    Worked on insertOrder() function that adds songs in the correct space in alphbetical order, not yet working.

--- list.c
+++ list.c
@@ -10,10 +10,10 @@ int main(){
     //will need later
     //songNode *table[26];
     printf("\n");
-    struct songNode *list = (struct songNode *) malloc(sizeof(*list));
-    list->artist[0] = 'b';
-    list->name[0] = 'a';
+    struct songNode *list = 0;
     list = insertFront(list, "Jim", "Kirk");
+    list = insertFront(list, "Bones", "McCoy");
+    list = insertOrder(list, "Spock", "Vulcan"); //NOT YET WORKING
     printList(list);
     printf("\n");
     return 0;
@@ -21,18 +21,32 @@ int main(){
 
 void printList(struct songNode *node){
     while (node->next){
-	printf("Name: %s\n", node->name);
 	printf("Artist: %s\n", node->artist);
+	printf("Name: %s\n", node->name);
 	node = node->next;
     }
-    printf("Name: %s\n", node->name);
-    printf("Artist: %s\n", node->artist);   
+    printf("Artist: %s\n", node->artist);
+    printf("Name: %s\n", node->name);   
 }
 
-struct songNode * insertFront(struct songNode * node, char name[256], char artist[256]){
+struct songNode * insertFront(struct songNode * node, char artist[256], char name[256]){
     struct songNode *new = (struct songNode *) malloc(sizeof(*new));
     strcpy(new->name, name);
     strcpy(new->artist, artist);
     new->next = node;
     return new;
+}
+
+struct songNode * insertOrder(struct songNode * node, char artist[256], char name[256]){
+    struct songNode *first = node; 
+    struct songNode *new = (struct songNode *) malloc(sizeof(*new));
+    strcpy(new->name, name);
+    strcpy(new->artist, artist);
+    while(node->next && strcmp(artist, node->artist) < 0){
+	node = node->next;
+    }
+    struct songNode *temp = node->next;
+    node->next = new;
+    new->next = temp; 
+    return first;
 }
--- list.h
+++ list.h
@@ -1,8 +1,9 @@
 typedef struct songNode{
-    char name[256];
     char artist[256];
+    char name[256];
     struct songNode *next;
 }songNode;
 
 void printList(struct songNode *);
 struct songNode * insertFront(struct songNode *, char[256], char[256]);
+struct songNode * insertOrder(struct songNode *, char[256], char[256]);
